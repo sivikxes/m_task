b23331a7f0cefc32604ed3d5bbcfccfd8316ba85
sivikxes <sivik.xes@gmail.com>
2016-12-14 18:07:20 +0200

    Update main.js

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -8,4 +8,39 @@ $(function () {
             $header.removeClass('scrolled');
         }
     });
+    $doc.on('click', function (e) {
+        if($(e.target).closest('.nav').length == 0)
+        $nav.removeClass('opened');
+    });
+    var $nav = $('.nav');
+    $nav.on('click', function () {
+        $nav.toggleClass('opened');
+    });
+
+    var $tabsLink = $(".tabs a");
+
+    var mySwiper = new Swiper('.swiper-container',{
+        mode:'horizontal',
+        loop: true,
+        calculateHeight:true,
+        onSlideChangeEnd: function (e) {
+            $tabsLink.removeClass('active');
+            $($tabsLink.get(e.activeLoopIndex)).addClass('active');
+        }
+    });
+
+    $tabsLink.on('touchstart mousedown',function(e){
+        e.preventDefault();
+        mySwiper.swipeTo( $(this).index() );
+    }).click(function(e){
+        e.preventDefault()
+    });
+
+    $('.section-btn').on('click', function (e) {
+        e.preventDefault();
+        var $needEl = $($(this).attr('data-href'));
+
+        $('body').animate({scrollTop:$needEl.position().top}, '500');
+    });
+
 });
